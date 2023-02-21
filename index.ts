3d82bc66ee396309f77e8209a4f0b912fd714df9
IBLINETUM\yonar.ramos <yonar.ramos@inetum.com>
2023-02-21 17:15:00 +0100

    enable api feature

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 const sqlite = require("./src/sqlite")
 const errors = require("./src/errors")
 const logger = require("./src/logger")
+const api = require("./src/api")
 
-module.exports = { errors, logger, sqlite }
+module.exports = { errors, logger, sqlite, api }
